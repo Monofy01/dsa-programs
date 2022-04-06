eec0645a27585886b23ba56ec6dc16ef6dccc010
monofy01 <bri_riva@hotmail.com>
2022-04-06 15:50:35 -0500

    StackLinked - Push/Pop Function

---
+++ 08-Stack/stackLinked.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+}*top=NULL;
+
+void push(int x) {
+    struct Node *t;
+    t = (struct Node *)malloc(sizeof(struct Node));
+
+    if(t==NULL)
+        printf("Stack is full\n");
+    else {
+        t->data = x;
+        t->next = top;
+        top = t;
+    }
+}
+
+int pop() {
+    struct Node *t;
+    int x = -1;
+
+    if(top==NULL)
+        printf("Stack is Empty\n");
+    else {
+        t = top;
+        top = top->next;
+        x = t->data;
+        free(t);
+    }
+    return x;
+}
+
+
+
+
+
+int main() {
+
+}
