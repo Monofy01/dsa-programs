0c48d760ad6e2aa6b6b64bfeff1b736d6cae6dd7
monofy01 <bri_riva@hotmail.com>
2022-04-14 07:05:26 -0500

    Queue - Enqueue Function

--- 09-Queue-ADT/queueArray.c
+++ 09-Queue-ADT/queueArray.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>git
 
 struct Queue {
     int size;
@@ -11,6 +12,15 @@ void create(struct Queue *q, int size) {
     q->size = size;
     q->front=q->rear=-1;
     q->Q=(int *)malloc(q->size*sizeof(int));
+}
+
+void enqueue(struct Queue *q, int x) {
+    if(q->rear == q->size-1)
+        printf("Queue is Full");
+    else {
+        q->rear++;
+        q->Q[q->rear] = x;
+    }
 }
 
 int main() {
